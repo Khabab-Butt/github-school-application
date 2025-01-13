c4dc1d9bb33b3ac5312c7c169ad19f915cf0e1bd
Khabab <khababbutt1228@gmail.com>
2025-01-13 11:42:34 +0500

    School App code pushed till Integration Tests.

---
+++ com.example.school/src/it/java/com/example/school/controller/SchoolControllerIT.java
@@ -0,0 +1,82 @@
+package com.example.school.controller;
+
+import static org.mockito.Mockito.*;
+import static java.util.Arrays.asList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.school.model.Student;
+import com.example.school.repository.StudentRepository;
+import com.example.school.repository.mongo.StudentMongoRepository;
+import com.example.school.view.StudentView;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+/**
+ * Communicates with a MongoDB server on localhost; start MongoDB with Docker
+ * with
+ * 
+ * <pre>
+ * docker run -p 27017:27017 --rm mongo:4.4.3
+ * </pre>
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class SchoolControllerIT {
+
+	@Mock
+	private StudentView studentView;
+
+	private StudentRepository studentRepository;
+
+	private SchoolController schoolController;
+
+	private AutoCloseable closeable;
+
+	private static int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
+
+	@Before
+	public void setUp() {
+		closeable = MockitoAnnotations.openMocks(this);
+		studentRepository = new StudentMongoRepository(new MongoClient(new ServerAddress("localhost", mongoPort)));
+		// explicit empty the database through the repository
+		for (Student student : studentRepository.findAll()) {
+			studentRepository.delete(student.getId());
+		}
+		schoolController = new SchoolController(studentView, studentRepository);
+	}
+
+	@After
+	public void releaseMocks() throws Exception {
+		closeable.close();
+	}
+
+	@Test
+	public void testAllStudents() {
+		Student student = new Student("1", "test");
+		studentRepository.save(student);
+		schoolController.allStudents();
+		verify(studentView).showAllStudents(asList(student));
+	}
+
+	@Test
+	public void testNewStudent() {
+		Student student = new Student("1", "test");
+		schoolController.newStudent(student);
+		verify(studentView).studentAdded(student);
+	}
+
+	@Test
+	public void testDeleteStudent() {
+		Student studentToDelete = new Student("1", "test");
+		studentRepository.save(studentToDelete);
+		schoolController.deleteStudent(studentToDelete);
+		verify(studentView).studentRemoved(studentToDelete);
+	}
+
+}
